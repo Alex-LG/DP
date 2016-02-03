84415626de3fd29099cd934484b733490f95bd62
alex-lg <alexlobanov88@gmail.com>
2016-02-03 14:32:36 +0400

    Mediator

---
+++ Patterns/Mediator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Mediator
+{
+    class Program
+    {        
+        static void Main()
+        {
+            ConcreteMediator m = new ConcreteMediator();
+
+            ConcreteColleague1 c1 = new ConcreteColleague1(m);
+            ConcreteColleague2 c2 = new ConcreteColleague2(m);
+
+            m.Colleague1 = c1;
+            m.Colleague2 = c2;
+
+            c1.Send("How are you?");
+            c2.Send("Fine, thanks");
+     
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Mediator
+    {
+        public abstract void Send(string message, Colleague colleague);
+    }
+
+    class ConcreteMediator : Mediator
+    {
+        public ConcreteColleague1 Colleague1 { private get; set; }
+
+        public ConcreteColleague2 Colleague2 { private get; set; }
+
+        public override void Send(string message, Colleague colleague)
+        {
+            if (colleague == Colleague1)
+            {
+                Colleague2.Notify(message);
+            }
+            else
+            {
+                Colleague1.Notify(message);
+            }
+        }
+    }
+
+    abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+    }
+
+    class ConcreteColleague1 : Colleague
+    {
+        public ConcreteColleague1(Mediator mediator) : base(mediator) { }
+
+        public void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+
+        public void Notify(string message)
+        {
+            Console.WriteLine("Colleague1 gets message: " + message);
+        }
+    }
+
+    class ConcreteColleague2 : Colleague
+    {
+        public ConcreteColleague2(Mediator mediator) : base(mediator) { }
+
+        public void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+
+        public void Notify(string message)
+        {
+            Console.WriteLine("Colleague2 gets message: " + message);
+        }
+    }
+}
