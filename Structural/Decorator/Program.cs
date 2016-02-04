31c463cb09fcfd926f1c44c28c23447347f0e676
alex-lg <alexlobanov88@gmail.com>
2016-02-04 11:52:14 +0400

    Decorator

---
+++ Structural/Decorator/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main()
+        {
+            ConcreteComponent c = new ConcreteComponent();
+            ConcreteDecoratorA dA = new ConcreteDecoratorA();
+            ConcreteDecoratorB dB = new ConcreteDecoratorB();
+
+            dA.SetComponent(c);
+            dB.SetComponent(dA);
+
+            dB.Operation();
+
+            Console.Read();
+        }
+    }
+
+
+    abstract class Component
+    {
+        public abstract void Operation();
+    }
+
+    class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteComponent.Operation()");
+        }
+    }
+
+    abstract class Decorator : Component
+    {
+        protected Component component;
+
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+
+    class ConcreteDecoratorA : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+
+            Console.WriteLine("ConcreteDecoratorA.Operation()");
+        }
+    }
+
+    class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddedBehavior();
+            Console.WriteLine("ConcreteDecoratorB.Operation()");
+        }
+
+        public void AddedBehavior()
+        {
+            Console.WriteLine("ConcreteDecoratorB.AddedBehavior()");
+        }
+    }
+}
