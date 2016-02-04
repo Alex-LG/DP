e0d0fe43a20c44ab39056ad5e178d2ec933684ab
alex-lg <alexlobanov88@gmail.com>
2016-02-04 09:54:21 +0400

    Adapter

---
+++ Structural/Adapter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Target target = new Adapter();
+            target.Request();
+            
+            Console.ReadKey();
+        }
+    }
+
+    class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Called Target Request");
+        }    
+    }
+
+    class Adapter : Target
+    {
+        Adaptee adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            // Do some work,
+            // then : 
+            adaptee.SpecificRequest();
+        }
+    }
+
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Called Specific Request");
+        }
+    }
+}
