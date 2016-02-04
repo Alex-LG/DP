ad0f90941157db505e5964eff562fe52734a9fad
alex-lg <alexlobanov88@gmail.com>
2016-02-04 10:24:36 +0400

    Bridge

---
+++ Structural/Bridge/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Abstraction ab = new RefinedAbstraction();
+            
+            ab.Implementor = new ConcreteImplementorA();
+            ab.Operation();
+            
+            ab.Implementor = new ConcreteImplementorB();
+            ab.Operation();
+            
+            Console.ReadKey();
+        }
+    }
+
+    class Abstraction
+    {
+        protected Implementor implementor;
+
+        public Implementor Implementor
+        {
+            set { implementor = value; }
+        }
+
+        public virtual void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+
+    class RefinedAbstraction : Abstraction
+    {
+        public override void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+
+    abstract class Implementor
+    {
+        public abstract void Operation();
+    }
+
+    class ConcreteImplementorA : Implementor
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("Hello from implementor A");
+        }
+    }
+
+    class ConcreteImplementorB : Implementor
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("Hello from implementor B");
+        }    
+    }
+}
