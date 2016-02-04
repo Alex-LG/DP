ff42b52dd8f01e526473a586d51be29ee1d64f83
alex-lg <alexlobanov88@gmail.com>
2016-02-04 17:01:52 +0400

    Singleton

---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+       
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            } 
+      
+            Console.ReadKey();
+        }
+    }
+
+    class Singleton
+    {
+        private static Singleton _instance;
+        
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            if (_instance == null)      // NOT thread-safe
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
